ff1ae80c691c8752206030a0bc8cc82545f53541
Oleksandr Vlasenko <oleksvlas@gmail.com>
2019-10-21 00:04:50 +0300

    Some change to examples with loops

---
+++ loops/for_in/for_in.js
@@ -0,0 +1,9 @@
+let menu ={
+    width: 300,
+    height: 200,
+    title: "Menu"
+};
+
+for (let key in menu) {
+    console.log("Ключ: " + key + " Значення: " + menu[key]);
+}
---
+++ loops/for_in/for_in_2.js
@@ -0,0 +1,11 @@
+let menu ={
+    width: 300,
+    height: 200,
+    title: "Menu"
+};
+let counter = 0;
+
+for (let key in menu) {
+    counter++;
+}
+console.log(counter);
