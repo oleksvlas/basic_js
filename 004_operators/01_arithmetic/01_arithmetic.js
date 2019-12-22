39c354abad0d5ecbc10d9866bc6f625e65e1c57e
Oleksandr Vlasenko <oleksvlas@gmail.com>
2019-12-23 00:24:35 +0200

    Add basic JavaScript Examples 2

---
+++ 004_operators/01_arithmetic/01_arithmetic.js
@@ -0,0 +1,26 @@
+let a = 9;
+let b = 5;
+let result;
+
+// Додавання (+)
+result = a + b;
+console.log("Результ додавання a+b: " + result);
+
+// Віднімання (-)
+result = a - b;
+console.log("Результ віднімання a-b: " + result);
+
+// Множення (*)
+result = a * b;
+console.log("Результ множення a*b: " + result);
+
+// Ділення (/)
+result = a / b;
+console.log("Результ ділення a/b: " + result);
+
+// Залишок від ділення (%)
+result = a / b;
+console.log("Результ залишку від ділення a/b: " + result);
+console.log( 5 % 2 ); // 1, залишок від ділення 5 на 2
+console.log( 8 % 3 ); // 2, залишок від ділення 8 на 3
+console.log( 6 % 3 ); // 0, залишок від ділення 6 на 3
---
+++ 004_operators/01_arithmetic/03_increment_decrement.js
@@ -0,0 +1,20 @@
+let num = 5;
+console.log("Інкремент");
+console.log("Постфіксний інкремент");
+console.log(num);   // виведе 5
+num++;
+console.log(num);  // виведе 6
+console.log(num++); // виведе 6
+console.log(num);  // виведе 7
+
+console.log('------------------------------------------');
+
+console.log('Префіксний інкремент');
+console.log(num);  // виведе 7
+++num;
+console.log(num);   // виведе 8
+console.log(++num); // виведе 9
+console.log(num);   // виведе 9
+
+console.log('------------------------------------------');
+
